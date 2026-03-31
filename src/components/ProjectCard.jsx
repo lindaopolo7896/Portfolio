53c792a4d7b49b64b0ce4c7f87151bc1580c8899
lindaopolo7896 <eleanor.opolo@strathmore.edu>
2026-03-31 15:55:54 +0300

    updated

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -12,7 +12,7 @@ function ProjectCard({ project }) {
       className="relative  rounded-2xl overflow-hidden group cursor-pointer h-auto"
     >
       <div
-        className="flex flex-col justify-end bg-cover bg-center h-auto min-h-140 lg:min-h-125 shadow-xl shadow-[#14B8A6] "
+        className="flex flex-col justify-end bg-cover bg-center h-auto min-h-150  shadow-xl shadow-[#14B8A6] "
         style={{
           backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${project.image})`,
         }}
@@ -50,54 +50,55 @@ function ProjectCard({ project }) {
 
           transition-all duration-500
 
-          flex flex-col p-6
+          flex flex-col
 
           
         "
       >
-        <div className="h-[100px] w-[100px]">
-          <img
-            src={project.image}
-            alt=""
-            className="w-full h-full rounded-2xl"
-          />
-        </div>
-        <h2 className="text-[#00d1c1] text-2xl font-bold mb-4 mt-2">
-          {project.title}
-        </h2>
-
-        <p className="text-white/80">{project.description}</p>
-
-        <button className="w-full border-2 border-[#14b8a6] text-[#14b8a6] mt-6 mb-4 py-2 rounded-full text-lg font-semibold hover:bg-white hover:border-0 transition-all duration-300 ease-in-out">
-          <a href={project.liveLink} target="_blank">
-            Live Preview
-          </a>
-        </button>
-
-        <div className="flex gap-4 justify-between mt-2">
-          <a
-            href={project.figmaLink}
-            target="_blank"
-            className="
+        <div
+          className=" w-full h-70 bg-cover bg-center flex items-end px-4 pb-2"
+          style={{
+            backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${project.image}) `,
+          }}
+        >
+          <div className="flex gap-4 justify-between mt-2 w-full px-6 bg-[#00141D] rounded-full">
+            <a
+              href={project.figmaLink}
+              target="_blank"
+              className="
                py-2 rounded-lg font-semibold 
               text-[#00d1c1]
               text-lg
               hover:opacity-80 transition
             "
-          >
-            Figma
-          </a>
+            >
+              Figma
+            </a>
 
-          <a
-            href={project.githubLink}
-            target="_blank"
-            className="
+            <a
+              href={project.githubLink}
+              target="_blank"
+              className="
                py-2 rounded-lg font-semibold text-white
               
             "
-          >
-            <FaGithub className="text-[30px] hover:text-[#00d1c1]" />
-          </a>
+            >
+              <FaGithub className="text-[30px] hover:text-[#00d1c1]" />
+            </a>
+          </div>
+        </div>
+        <div className="px-6 py-2">
+          <h2 className="text-[#00d1c1] text-2xl font-bold mb-4 mt-2">
+            {project.title}
+          </h2>
+
+          <p className="text-white/80">{project.description}</p>
+
+          <button className="w-full border-2 border-[#14b8a6] text-[#14b8a6] mt-6 mb-4 py-2 rounded-full text-lg font-semibold hover:bg-white hover:border-0 transition-all duration-300 ease-in-out">
+            <a href={project.liveLink} target="_blank">
+              Live Preview
+            </a>
+          </button>
         </div>
       </div>
     </motion.div>
